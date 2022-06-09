d831baf6a9337ffba8516bfae89c2da99f0b900c
wmm <549240943@qq.com>
2022-06-09 15:53:10 +0800

    fix

--- febs-register/src/main/java/com/dm/febs/register/config/FebsRegisterWebSecurityConfigure.java
+++ febs-register/src/main/java/com/dm/febs/register/config/FebsRegisterWebSecurityConfigure.java
@@ -1,8 +1,10 @@
 package com.dm.febs.register.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
 public class FebsRegisterWebSecurityConfigure extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
